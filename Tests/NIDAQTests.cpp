0ae7b7ff365fbc22394eb450506e24c1b652d97e
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2024-10-13 23:50:45 -0700

    Update NIDAQTests.cpp

--- Tests/NIDAQTests.cpp
+++ Tests/NIDAQTests.cpp
@@ -86,163 +86,12 @@ protected:
                 numChannels,
                 100,
                 0.0f });
+
+        nidaqThread = tester->createDataThread<NIDAQThread>();
     }
 
     void TearDown() override
     {
-    }
-
-    void SetupMockCalls()
-    {
-        //ON_CALL (apiWrapper, getSysDevNames (_, _))
-        //    .WillByDefault ([] (char* data, NIDAQmxAPI::uInt32 bufferSize)
-        //                    {
-        //    strcpy(data, "Dev1");
-        //    LOGC("getSysDevNames called ", String(data));
-        //    return 0; });
-        //ON_CALL (apiWrapper, getDevProductType (_, _, _))
-        //    .WillByDefault ([] (const char device[], char* data, NIDAQmxAPI::uInt32 bufferSize)
-        //                    {
-        //    strcpy(data, "Dev1");
-        //    return 0; });
-        //EXPECT_CALL (apiWrapper, readAnalogF64 (_, _, _, _, _, _, _, _))
-        //    .WillOnce ([&] (NIDAQmxAPI::TaskHandle taskHandle,
-        //                         NIDAQmxAPI::int32 numSampsPerChan,
-        //                         NIDAQmxAPI::float64 timeout,
-        //                         NIDAQmxAPI::bool32 fillMode,
-        //                         NIDAQmxAPI::float64* readArray,
-        //                         NIDAQmxAPI::uInt32 arraySizeInSamps,
-        //                         NIDAQmxAPI::int32* sampsPerChanRead,
-        //                         NIDAQmxAPI::bool32* reserved)
-        //                    {
-        //    numSampsPerChan = numSamplesPerChannel;
-
-        //    for (int i = 0; i < numChannels; i++)
-        //    {
-        //        for(int j = 0; j < numSamplesPerChannel; j++)
-        //        {
-        //            readArray[i * numSamplesPerChannel + j] = i + j;
-        //            sampleNumbers[i * numSamplesPerChannel + j] = static_cast<juce::int64> (i) + j;
-        //        }
-        //    }
-
-        //    arraySizeInSamps = numChannels * numSamplesPerChannel;
-        //    *sampsPerChanRead = numSamplesPerChannel;
-
-        //    return 0; });
-        //ON_CALL (apiWrapper, readDigitalU8 (_, _, _, _, _, _, _, _)).WillByDefault ([&] (NIDAQmxAPI::TaskHandle taskHandle, NIDAQmxAPI::int32 numSampsPerChan, NIDAQmxAPI::float64 timeout, NIDAQmxAPI::bool32 fillMode, NIDAQmxAPI::uInt8* readArray, NIDAQmxAPI::uInt32 arraySizeInSamps, NIDAQmxAPI::int32* sampsPerChanRead, NIDAQmxAPI::bool32* reserved)
-        //                                                                            {
-        //    numSampsPerChan = numSamplesPerChannel;
-
-        //    for (int i = 0; i < numChannels; i++)
-        //    {
-        //        for(int j = 0; j < numSamplesPerChannel; j++)
-        //        {
-        //            readArray[i * numSamplesPerChannel + j] = i + j;
-        //            sampleNumbers[i * numSamplesPerChannel + j] = static_cast<juce::int64> (i) + j;
-        //        }
-        //    }
-
-        //    arraySizeInSamps = numChannels * numSamplesPerChannel;
-        //    *sampsPerChanRead = numSamplesPerChannel;
-
-        //    return 0; });
-        //ON_CALL (apiWrapper, readDigitalU32 (_, _, _, _, _, _, _, _)).WillByDefault ([&] (NIDAQmxAPI::TaskHandle taskHandle, NIDAQmxAPI::int32 numSampsPerChan, NIDAQmxAPI::float64 timeout, NIDAQmxAPI::bool32 fillMode, NIDAQmxAPI::uInt32* readArray, NIDAQmxAPI::uInt32 arraySizeInSamps, NIDAQmxAPI::int32* sampsPerChanRead, NIDAQmxAPI::bool32* reserved)
-        //                                                                             {
-        //    numSampsPerChan = numSamplesPerChannel;
-
-        //    for (int i = 0; i < numChannels; i++)
-        //    {
-        //        for(int j = 0; j < numSamplesPerChannel; j++)
-        //        {
-        //            readArray[i * numSamplesPerChannel + j] = i + j;
-        //            sampleNumbers[i * numSamplesPerChannel + j] = static_cast<juce::int64> (i) + j;
-        //        }
-        //    }
-
-        //    arraySizeInSamps = numChannels * numSamplesPerChannel;
-        //    *sampsPerChanRead = numSamplesPerChannel;
-
-        //    return 0; });
-        //ON_CALL (apiWrapper, readDigitalU16 (_, _, _, _, _, _, _, _)).WillByDefault ([&] (NIDAQmxAPI::TaskHandle taskHandle, NIDAQmxAPI::int32 numSampsPerChan, NIDAQmxAPI::float64 timeout, NIDAQmxAPI::bool32 fillMode, NIDAQmxAPI::uInt16* readArray, NIDAQmxAPI::uInt32 arraySizeInSamps, NIDAQmxAPI::int32* sampsPerChanRead, NIDAQmxAPI::bool32* reserved)
-        //                                                                             {
-        //        numSampsPerChan = numSamplesPerChannel;
-
-        //        for (int i = 0; i < numChannels; i++)
-        //        {
-        //            for(int j = 0; j < numSamplesPerChannel; j++)
-        //            {
-        //                readArray[i * numSamplesPerChannel + j] = i + j;
-        //                sampleNumbers[i * numSamplesPerChannel + j] = static_cast<juce::int64> (i) + j;
-        //            }
-        //        }
-
-        //        arraySizeInSamps = numChannels * numSamplesPerChannel;
-        //        *sampsPerChanRead = numSamplesPerChannel;
-
-        //        return 0; });
-
-        //ON_CALL (apiWrapper, getDevProductCategory (_, _))
-        //    .WillByDefault ([&] (const char[], NIDAQmxAPI::int32* data)
-        //                    {
-        //        *data = 1;  // Example category
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevProductNum (_, _))
-        //    .WillByDefault ([&] (const char[], NIDAQmxAPI::uInt32* data)
-        //                    {
-        //        *data = 12345;  // Example product number
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevSerialNum (_, _))
-        //    .WillByDefault ([&] (const char[], NIDAQmxAPI::uInt32* data)
-        //                    {
-        //        *data = 12345;  // Example serial number
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevAISimultaneousSamplingSupported (_, _))
-        //    .WillByDefault ([&] (const char[], NIDAQmxAPI::bool32* data)
-        //                    {
-        //        *data = false;  // Example simultaneous sampling supported
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevAIMinRate (_, _))
-        //    .WillByDefault ([&] (const char[], NIDAQmxAPI::float64* data)
-        //                    {
-        //        *data = 1000.0f;  // Example minimum rate
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevAIMaxSingleChanRate (_, _))
-        //    .WillByDefault ([] (const char[], NIDAQmxAPI::float64* data)
-        //                    {
-        //        *data = 30000.0f;  // Example maximum single channel rate
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevAIMaxMultiChanRate (_, _))
-        //    .WillByDefault ([&] (const char[], NIDAQmxAPI::float64* data)
-        //                    {
-        //        *data = 20000.0f;  // Example maximum multi-channel rate
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevAIVoltageRngs (_, _, _))
-        //    .WillByDefault ([&] (const char[], NIDAQmxAPI::float64* data, NIDAQmxAPI::uInt32 arraySizeInElements)
-        //                    {
-        //        data[0] = -100.0f;
-        //        data[1] = 100.0f;
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevAIPhysicalChans (_, _, _))
-        //    .WillByDefault ([] (const char[], char* data, NIDAQmxAPI::uInt32 bufferSize)
-        //                    {
-        //        strcpy (data, "Dev1/ai0,Dev1/ai1,Dev1/ai2,Dev1/ai3,Dev1/ai4,Dev1/ai5,Dev1/ai6,Dev1/ai7");
-        //        return 0; });
-        //ON_CALL (apiWrapper, getPhysicalChanAITermCfgs (_, _))
-        //    .WillByDefault ([] (const char[], NIDAQmxAPI::int32* data)
-        //                    {
-        //        *data = DAQmx_Val_RSE;  // Example terminal configuration
-        //        return 0; });
-        //ON_CALL (apiWrapper, getAIResolution (_, _, _))
-        //    .WillByDefault ([&] (NIDAQmxAPI::TaskHandle taskHandle, const char channel[], NIDAQmxAPI::float64* data)
-        //                    {
-        //        *data = 12.0f;  // Example resolution
-        //        return 0; });
-        //ON_CALL (apiWrapper, getDevDILines (_, _, _))
-        //    .WillByDefault ([] (const char device[], char* data, NIDAQmxAPI::uInt32 bufferSize)
-        //                    {
-        //        strcpy(data, "Dev1/port0/line0,Dev1/port0/line1,Dev1/port0/line2,Dev1/port0/line3,Dev1/port0/line4,Dev1/port0/line5,Dev1/port0/line6,Dev1/port0/line7");  // Example digital lines
-        //        return 0; });
     }
 
     AudioBuffer<float> createBuffer (float startingValue, float step, int numChannels, int numSamples)
@@ -304,13 +153,8 @@ protected:
 protected:
     NIDAQThread* nidaqThread;
     std::unique_ptr<DataThreadTester> tester;
-    static constexpr NIDAQmxAPI::int32 numChannels = 16;
-    static constexpr NIDAQmxAPI::int32 numSamplesPerChannel = 4;
+    NIDAQmxAPI::int32 numChannels = 16;
     int64_t currentSampleIndex = 0;
-    MockNIDAQmxAPI apiWrapper;
-    
-    int64 sampleNumbers[numChannels * numSamplesPerChannel];
-    NIDAQmxAPI::float64 readArray[numChannels * numSamplesPerChannel];
 };
 
 /*
@@ -324,7 +168,7 @@ TEST_F (NIDAQIntegrationTests, DataIntegrity)
     tester->startAcquisition (false);
 
     int numSamples = 100;
-    auto inputBuffer = createBuffer (1000.0, 20.0, 5, numSamples);
+    auto inputBuffer = createBuffer (1000.0, 20.0, numChannels, numSamples);
     writeBlock (inputBuffer);
 
     tester->stopAcquisition();
